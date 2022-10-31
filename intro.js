9c870e737ceb392f42a943aafd16252004a2c6f2
profalexanderalbuquerque <profalexander.albuquerque@fiap.com.br>
2022-10-31 09:56:23 -0300

    atualizado

---
+++ intro.js
@@ -0,0 +1,31 @@
+var nomeSistema = "Nome do Sistema";
+let nome = "Alexander Gobbato Paulino Albuquerque";
+let idade = 42;
+let professor = true;
+let opcao = 'A';
+
+console.log(nomeSistema);
+console.log("Tipo do dado nome: " + typeof(nome));
+console.log("Tipo do idade: " + typeof(idade));
+console.log("Tipo do dado professor xxx : " + typeof(professor));
+/*
++ soma
+- subtracao
+/ divisão
+* multiplicacao
+% resto da divisao
+*/
+
+let num1 = 5;
+let num2 = 11;
+
+console.log("Soma: " + (num1 + num2));
+console.log("Sub: " + (num1 - num2));
+console.log("Mul: " + (num1 * num2));
+console.log("Div: " + (num1 / num2));
+console.log("Resto: " + (num2 % num1));
+
+document.write("<h1>Olá</h1>");
+document.write("<h2>Olá</h2>");
+
+
